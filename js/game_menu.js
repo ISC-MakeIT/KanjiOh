e15492f6dc1270d6a5c179c099eb932040ab96ff
s61t6ftaro <bisk1280392@gn.iwasaki.ac>
2021-10-03 16:11:36 +0900

    button add setIntractive

--- js/game_menu.js
+++ js/game_menu.js
@@ -10,6 +10,7 @@ export default class GameMenu extends Phaser.Scene {
     this.load.image("cloud", "../img/game_cloud.png");
     this.load.image("tree", "../img/tree.png");
     this.load.image("top_mogura", "../img/mogura.png");
+    this.load.image("mogura", "../img/min_mogura.png");
 
     // bgm
     this.load.audio("top_bgm", "../audio/top.mp3");
@@ -42,26 +43,40 @@ export default class GameMenu extends Phaser.Scene {
     const mogura = this.add.image(750, 530, "top_mogura");
     mogura.depth = 1;
 
+    const miniMogura1 = this.add.sprite(90, 280, "mogura");
+    miniMogura1.depth = 3;
+
+    const miniMogura2 = this.add.sprite(90, 410, "mogura");
+    miniMogura2.depth = 3;
+
+    const miniMogura3 = this.add.sprite(90, 540, "mogura");
+    miniMogura3.depth = 3;
+
     // 背景描画
     const bgGameMenu = this.add.graphics();
     bgGameMenu.fillStyle(0xebfdff, 1).fillRect(0, 0, 1024, 768);
 
     // 音声アイコン枠描画
     const soundCircle = this.add.graphics();
-    soundCircle.fillStyle(0x333333, 1).fillCircle(70, 700, 40).depth = 3;
+    soundCircle
+      .fillStyle(0x333333, 1)
+      .fillCircle(70, 700, 40)
+      .setInteractive(
+        new Phaser.Geom.Circle(70, 700, 40),
+        Phaser.Geom.Circle.Contains)
+      .depth = 3;
 
     // 音声アイコン
-
     const soundIcon = this.add.sprite(70, 700, "sound");
     let soundStatus = 1;
-    soundIcon.setInteractive().depth = 4;
+    soundIcon.depth = 4;
 
     // 音楽
     const gameBgm = this.sound.add("top_bgm");
     gameBgm.allowMultiple = false;
     gameBgm.play();
 
-    soundIcon.on(
+    soundCircle.on(
       "pointerdown",
       () => {
         if (soundStatus === 0) {
@@ -84,16 +99,11 @@ export default class GameMenu extends Phaser.Scene {
       .lineStyle(5, 0x645246)
       .fillStyle(0xffffff, 1)
       .fillRoundedRect(30, 100, 350, 90, 45)
-      .strokePath().depth = 1;
+      .strokePath()
+      .setInteractive(new Phaser.Geom.Rectangle(30, 100, 350, 90), Phaser.Geom.Rectangle.Contains)
+      .depth = 2;
 
-    const fndDiffText = this.add.text(70, 130, "羊の中に犬が一匹", {
-      fontSize: "32px",
-      fill: "#333333",
-    });
-
-    fndDiffText
-      .setInteractive()
-      .setPadding(4)
+    fndDiffButton
       .on(
         "pointerdown",
         () => {
@@ -101,23 +111,38 @@ export default class GameMenu extends Phaser.Scene {
           this.scene.start("game_setting");
         },
         this
-      ).depth = 2;
+        )
 
-    // 作成中にする
+    const fndDiffText = this.add
+      .text(70, 130, "羊の中に犬が一匹", {
+        fontSize: "32px",
+        fill: "#333333",
+      });
+
+    fndDiffText
+      .setPadding(4)
+      .depth = 3;
+      
+  // 作成中にする
     // 多言語
     const mnyLngButton = this.add.graphics();
     mnyLngButton
       .lineStyle(5, 0x645246)
       .fillStyle(0x32b65e, 1)
       .fillRoundedRect(30, 230, 350, 90, 45)
-      .strokePath().depth = 1;
+      .setInteractive(new Phaser.Geom.Rectangle(30, 230, 350, 90), Phaser.Geom.Rectangle.Contains)
+      .strokePath()
+      .depth = 2;
+ 
+    const mnyLngText = this.add
+      .text(150, 260, "作成中", {
+        fontSize: "32px",
+        fill: "#ffffff",
+      });
 
-    const mnyLngText = this.add.text(150, 260, "作成中", {
-      fontSize: "32px",
-      fill: "#ffffff",
-    });
-
-    mnyLngText.setPadding(4).depth = 2;
+    mnyLngText
+      .setPadding(4)
+      .depth = 3;
 
     // 神経衰弱
     const memoryGmButton = this.add.graphics();
@@ -125,14 +150,19 @@ export default class GameMenu extends Phaser.Scene {
       .lineStyle(5, 0x645246)
       .fillStyle(0x32b65e, 1)
       .fillRoundedRect(30, 360, 350, 90, 45)
-      .strokePath().depth = 1;
+      .setInteractive(new Phaser.Geom.Rectangle(30, 360, 350, 90), Phaser.Geom.Rectangle.Contains)
+      .strokePath()
+      .depth = 2;
 
-    const memoryText = this.add.text(150, 390, "作成中", {
-      fontSize: "32px",
-      fill: "#ffffff",
-    });
+    const memoryText = this.add
+      .text(150, 390, "作成中", {
+        fontSize: "32px",
+        fill: "#ffffff",
+      });
 
-    memoryText.setPadding(4).depth = 2;
+    memoryText
+      .setPadding(4)
+      .depth = 3;
 
     // 仲間で集まれ
     const tgtherFriendButton = this.add.graphics();
@@ -140,13 +170,18 @@ export default class GameMenu extends Phaser.Scene {
       .lineStyle(5, 0x645246)
       .fillStyle(0x32b65e, 1)
       .fillRoundedRect(30, 490, 350, 90, 45)
-      .strokePath().depth = 1;
+      .setInteractive(new Phaser.Geom.Rectangle(30, 490, 350, 90), Phaser.Geom.Rectangle.Contains)
+      .strokePath()
+      .depth = 2;
 
-    const tgtherText = this.add.text(150, 520, "作成中", {
-      fontSize: "32px",
-      fill: "#ffffff",
-    });
+    const tgtherText = this.add
+      .text(150, 520, "作成中", {
+        fontSize: "32px",
+        fill: "#ffffff",
+      });
 
-    tgtherText.setPadding(4).depth = 2;
+    tgtherText
+      .setPadding(4)
+      .depth = 3;
   }
 }
